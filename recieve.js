4956c17a92a9bd0c1aad4633fcc309401ca7ebdb
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-20 09:36:51 +0530

    created worker queues

--- recieve.js
+++ recieve.js
@@ -2,15 +2,19 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var q = 'task_queue';
     ch.assertQueue(q, {
-      durable: false,
+      durable: true,
     });
     console.log('Waiting for messages from queue');
     ch.consume(q, function(msg) {
-      console.log('Recieved message from queue : ', msg.content.toString());
+      var delaySeconds = msg.content.toString().split('.').length - 1;
+      console.log('Recieved ', msg.content.toString());
+      setTimeout(function() {
+        console.log('done the task ', msg.content.toString());
+      }, delaySeconds * 1000);
     }, {
-      noAck: true,
+      noAck: false,
     });
   });
 })
--- send.js
+++ send.js
@@ -3,15 +3,22 @@ var amqp = require('amqplib/callback_api');
 // Creating a connection
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var q = 'task_queue';
+    var msg = process.argv.slice(2).join(' ') || 'Hello World.........';
+
     ch.assertQueue(q, {
-      durable: false, // Look what is this
+      durable: true, // Look what is this
     });
-    ch.sendToQueue(q, new Buffer('Hello World'));
-    console.log('Sent Hello World');
+    var count = 0;
+
+    setInterval(() => {
+      count += 1;
+
+      ch.sendToQueue(q, new Buffer(count+msg), {
+        persistent: true,
+      });
+      console.log('Sender Sent ', msg);
+    }, 1000);
+
   });
-  setTimeout(function() {
-    conn.close();
-    process.exit(0);
-  }, 5000)
 });
